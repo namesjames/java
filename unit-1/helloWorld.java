ff87a5ab5f92ee8313c3f45509c39fc25db6c563
J ª/\/\ ∑ $ <james@galvanize.com>
2015-11-30 23:33:00 -0700

    Create helloWorld.java

---
+++ unit-1/helloWorld.java
@@ -0,0 +1,111 @@
+public class YourName {
+	public static void main(String[] args) {
+
+		System.out.println("James");
+
+	}
+}
+
+public class DataTypes {
+	public static void main(String[] args) {
+
+		System.out.println(2);
+
+	}
+}
+
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+public class DataTypesC {
+	public static void main(String[] args) {
+
+		System.out.println('J');
+
+	}
+}
+
+public class Variables {
+	public static void main(String[] args) {
+
+		int myNumber = 42;
+		boolean isFun = true; 
+		char movieRating = 'A'; 
+
+	}
+}
+
+public class WhiteSpace {
+	public static void main(String[] args) {
+
+		boolean isFormatted = false;
+    System.out.println(isFormatted);
+
+	}
+}
+
+public class Comments {
+	public static void main(String[] args) {
+
+		//System.out.println("Noise!");
+		
+/* hi
+hi
+hi
+hi
+*/
+	
+	}
+}
+
+public class Arithmetic {
+	public static void main(String[] args) {
+
+		int myNumber = 2 * 4;
+		System.out.println(myNumber);
+
+	}
+}
+
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 14 % 6;
+		System.out.println(myRemainder);
+
+	}
+} 
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+
+		System.out.println(2 > 1);
+
+	}
+}
+
+public class EqualityOperators {
+	public static void main(String[] args) {
+
+		System.out.println(true == true);
+
+	}
+}
+
+public class Generalizations {
+	public static void main(String[] args) {
+// 1337 code snipz
+
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;		
+		System.out.println(epicLevel);
+		
+
+	}
+}
