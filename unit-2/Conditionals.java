9ea35fd7d794a0563e0bace221c697bd37e46c0e
J ª/\/\ ∑ $ <james@galvanize.com>
2015-12-01 23:14:48 -0700

    Update Conditionals.java

--- unit-2/Conditionals.java
+++ unit-2/Conditionals.java
@@ -54,3 +54,145 @@ public class And {
 		
 	}
 }
+
+public class Or {
+	public static void main(String[] args) {
+
+		System.out.println(1 > 2 || 2 > 1);
+		
+	}
+}
+
+public class Not {
+	public static void main(String[] args) {
+
+		System.out.println(!false);
+		System.out.println(!(5>=1) );
+		
+	}
+}
+
+public class Precedence {
+	public static void main(String[] args) {
+
+		boolean riddle = !( 1 < 8 || (5 > 2 && 3 < 5));
+		System.out.println(riddle);
+
+	}
+}
+
+public class If {
+	public static void main(String[] args) {
+
+		if (2>1) {
+
+			System.out.println("Access granted.");
+
+		}
+		
+	}
+}
+
+public class IfElse {
+	public static void main(String[] args) {
+
+		if (8 <= 7) {
+
+			System.out.println("Try again...");
+
+		} else {
+
+			System.out.println("Success!");
+
+		}
+		
+	}
+}
+
+public class IfElseIf {
+	public static void main(String[] args) {
+
+		int round = 8;
+
+		if (round > 12) {
+
+			System.out.println("The match is over!");
+
+		} else if (round > 0) {
+
+			System.out.println("The match is underway!");
+
+		}	else {
+
+			System.out.println("The boxing match hasn't started yet.");
+
+		}	
+	}
+}
+
+public class Ternary {
+	public static void main(String[] args) {
+		
+		int fuelLevel = 3;
+
+		char canDrive = (fuelLevel > 0) ? 'Y' : 'N';
+		System.out.println(canDrive);
+
+	}
+}
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		char penaltyKick = 'L';
+
+		switch (penaltyKick) {
+
+			case 'L': System.out.println("Messi shoots to the left and scores!");
+								break; 
+			case 'R': System.out.println("Messi shoots to the right and misses the goal!");
+								break;
+			case 'C': System.out.println("Messi shoots down the center, but the keeper blocks it!");
+								break;
+			default:
+				System.out.println("Messi is in position...");
+
+		}
+
+	}
+}
+
+public class GeneralizationsB {
+	public static void main(String[] args) {
+
+	// ( 3 >= 3 && !(true || true) )
+	boolean tricky = false;	
+
+	if(2016 < 2015) {
+
+			System.out.println("Stuck in the past...");
+
+		}else {
+
+			System.out.println("Upgraded to the future!");
+
+		}
+	
+
+	int subwayTrain = 5;
+
+		switch (subwayTrain){
+
+			case 1 : System.out.println("This is a South Ferry bound train!");
+								break; 
+			case 5 : System.out.println("This is a Brooklyn bound train!");
+								break;
+			case 7 : System.out.println("This is a Queens bound train!");
+								break;
+			default:
+				System.out.println("I'm not sure where that train goes...");
+	
+		}
+
+	}
+}
