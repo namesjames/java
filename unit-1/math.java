35d294169587dfbc8d71fd23496f884242a1eb77
J ª/\/\ ∑ $ <james@galvanize.com>
2016-07-10 23:42:06 -0600

    Update math.java

--- unit-1/math.java
+++ unit-1/math.java
@@ -4,7 +4,11 @@ public class Magic {
 		int myNumber = 10;
 		int stepOne = myNumber * myNumber;
 		int stepTwo = stepOne + myNumber;
-		
-
+		int stepThree = stepTwo / myNumber;
+		int stepFour = stepThree + 17;
+		int stepFive = stepFour - myNumber;
+		int stepSix = stepFive / 6;
 	}
 }
+// This number will be printed to the console no matter what integer you choose as the original number! magic!
+System.out.println(stepSix);
